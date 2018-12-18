ed8ed82301b9ab1918f89bc5f180110aa8d6304f
ali raza <alirazame5@gmail.com>
2018-12-18 23:16:17 +0400

    Extra Themes Removed

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'alirazam_mohalla');
 define('DB_PASSWORD', 'Pakistan@143');
 
 /** MySQL hostname */
-define('DB_HOST', '192.185.41.202');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
